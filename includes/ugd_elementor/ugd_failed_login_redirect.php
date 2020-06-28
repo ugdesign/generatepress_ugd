c0d73527814b8008edc202fae1454eca04f36801
ugdesign <git@ugdesign.nz>
2020-06-29 09:34:48 +1200

    Change login redirect routine names to be ugd specific to avoid potential conficts

--- includes/ugd_elementor/ugd_failed_login_redirect.php
+++ includes/ugd_elementor/ugd_failed_login_redirect.php
@@ -12,8 +12,8 @@
 */
 
 // Redirect the user back to the login page after the login failed, and add a $_GET parameter to let us know. Courtesy of WordPressFlow.com
-add_action( 'wp_login_failed', 'elementor_form_login_fail', 9999999 );
-function elementor_form_login_fail( $username ) {
+add_action( 'wp_login_failed', 'ugd_elementor_form_login_fail', 9999999 );
+function ugd_elementor_form_login_fail( $username ) {
     $referrer = $_SERVER['HTTP_REFERER'];  // where did the post submission come from?
     // if there's a valid referrer, and it's not the default log-in screen
     if ((!empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') )) {
@@ -24,8 +24,8 @@ function elementor_form_login_fail( $username ) {
     }
 }
 // This is also important. Make sure that the redirect still runs if the username and/or password are empty.
-add_action( 'wp_authenticate', 'elementor_form_login_empty', 1, 2 );
-function elementor_form_login_empty( $username, $pwd ) {
+add_action( 'wp_authenticate', 'ugd_elementor_form_login_empty', 1, 2 );
+function ugd_elementor_form_login_empty( $username, $pwd ) {
     $referrer = $_SERVER['HTTP_REFERER'];  // where did the post submission come from?
  if ( empty( $username ) || empty( $pwd ) ) {
     if ((!strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') )) {
@@ -38,7 +38,7 @@ function elementor_form_login_empty( $username, $pwd ) {
  }
 }
 
-function generate_login_fail_messaging(){
+function ugd_generate_login_fail_messaging(){
     ob_start();
     if($_GET['login'] == 'failed'){
     echo '<div class="message_login_fail" style="background-color: #ca5151;color: #ffffff;display: block;margin-bottom: 20px;text-align: center;padding: 9px 15px; width: fit-content;margin: 0 auto;"><span style="color: #ca5151;background-color: #fff;width: 20px;height: 20px;display: inline-flex;align-items: center;justify-content: center;font-weight: 900;border-radius: 50%;margin-right: 10px;">!</span>Oops! Looks like you have entered the wrong username or password. Please check your login details and try again.</div>';
@@ -47,5 +47,5 @@ function generate_login_fail_messaging(){
     ob_end_clean();
     return $return_string;
 }
-add_shortcode('login_fail_messaging', 'generate_login_fail_messaging');
+add_shortcode('ugd_login_fail_messaging', 'ugd_generate_login_fail_messaging');
 ?>
